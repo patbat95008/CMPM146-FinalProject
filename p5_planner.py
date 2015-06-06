fab05812d0416a3f5c7073f5a5d3cc3651e84f77
Patrick <pcrussel@ucsc.edu>
2015-06-05 22:28:19 -0700

    Changed the crafting to events

--- p5_planner.py
+++ p5_planner.py
@@ -142,34 +142,10 @@ def inventory_to_set(d):
 goal_state = make_initial_state(Crafting['Goal'])
 
 def heuristic(state):
-	#state_score = 0
-   #Take the infinite if there are ever more items the necessary
-	 if state["bench"] > 1 or state["cart"] > 1 or state["furnace"] > 1 or state["iron_axe"] > 1 or state["iron_pickaxe"] > 1 or state["stone_axe"] > 1 or state["stone_pickaxe"] > 1 or state["wooden_axe"] > 1 or state["wooden_pickaxe"] > 1:
-          return sys.maxint
-
-
-	 if state["coal"] > 1 and state["coal"] > goal_state["coal"]:
-			 return sys.maxint
+	
+	
    
-	 if state["cobble"] > 8 and state["cobble"] > goal_state["cobble"]:
-			 return sys.maxint
-   
-	 if state["ingot"] > 6 and state["ingot"] > goal_state["ingot"]:
-			 return sys.maxint
-   
-	 if state["ore"] > 1 and state["ore"] > goal_state["ore"]:
-			 return sys.maxint
-   
-	 if state["plank"] > 4 and state["plank"] > goal_state["plank"]:
-			 return sys.maxint
-   
-	 if state["stick"] > 4 and state["stick"] > goal_state["stick"]:
-			 return sys.maxint
-      
-	 if state["wood"] > 1 and state["wood"] > goal_state["wood"]:
-			 return sys.maxint
-      
-	 return 0
+	return 0
 
 #This constructs the rules needed for the graph. This was given.
 for name,rule in Crafting['Recipes'].items():
